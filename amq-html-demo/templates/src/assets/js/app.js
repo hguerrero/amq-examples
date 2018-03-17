f4a0db09a4bbffc0f8b1a076e544c08001f0fac9
Hugo Guerrero <hguerrer@redhat.com>
2018-03-16 20:34:20 -0600

    update to stock quote

--- amq-html-demo/templates/src/assets/js/app.js
+++ amq-html-demo/templates/src/assets/js/app.js
@@ -1,6 +1,16 @@
 (function($, document, window){
 	
 	$(document).ready(function(){
+		$.rand = function(arg) {
+			if ($.isArray(arg)) {
+				return arg[$.rand(arg.length)];
+			} else if (typeof arg === "number") {
+				return Math.floor(Math.random() * arg);
+			} else {
+				return 4;  // chosen by fair dice roll
+			}
+		};
+		
 		$.fn.serializeObject = function() {
 			var o = {};
 			var a = this.serializeArray();
