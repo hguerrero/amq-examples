d8fd00599e7daac518ae9ea0ac5399f4ba5113d5
Duncan Doyle <Duncan.Doyle@gmail.com>
2019-10-15 22:33:44 -0400

    Implemented cheapest transport selection logic in Drools rules on Kogito.

--- kafka-streams-quarkus-rideshare/src/main/java/com/redhat/kafkastreams/rideshare/RideSharePipeline.java
+++ kafka-streams-quarkus-rideshare/src/main/java/com/redhat/kafkastreams/rideshare/RideSharePipeline.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
@@ -11,12 +12,15 @@ import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.ValueJoiner;
-import org.apache.kafka.streams.kstream.Windowed;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.TimeWindows;
+import org.apache.kafka.streams.kstream.ValueJoiner;
+import org.apache.kafka.streams.kstream.Windowed;
+import org.drools.core.ClassObjectFilter;
+import org.kie.api.runtime.KieSession;
+import org.kie.kogito.rules.KieRuntimeBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,6 +29,9 @@ public class RideSharePipeline {
     private static final Logger log = LoggerFactory.getLogger(RideSharePipeline.class);
 
     final Serde<Transport> transportSerde = new Transport();
+
+    @Inject
+    KieRuntimeBuilder krb;
 
     @Produces
     public Topology buildTopology() {
@@ -68,21 +75,31 @@ public class RideSharePipeline {
 			(key, value) -> log.info("Lyft => Spaces:" + key + " Cost: " + String.format("$ %3d", value.getCost()) + " USD")
         );
         
+        
+
+
+
         KTable<Windowed<Integer>, Transport> rides = uberCost.leftJoin(
 			lyftCost,
 			new ValueJoiner<Transport, Transport, Transport>() {
 				@Override
 				public Transport apply(Transport leftValue, Transport rightValue) {
-				  if (leftValue == null) return new Transport("no transport",0L,0,0,false);
-                  if (rightValue == null) return leftValue;
-				  return leftValue.getCost() > rightValue.getCost() ? rightValue : leftValue;
+				  //if (leftValue == null) return new Transport("no transport",0L,0,0,false);
+                  //if (rightValue == null) return leftValue;
+                    KieSession kSession = krb.newKieSession();
+                    kSession.insert(leftValue);
+                    kSession.insert(rightValue);
+                    kSession.fireAllRules();
+                    Transport selectedTransport = (Transport) kSession.getObjects(new ClassObjectFilter(Transport.class)).stream().findFirst().orElse(null);
+                    kSession.dispose();
+                    return selectedTransport;
+				    //return leftValue.getCost() > rightValue.getCost() ? rightValue : leftValue;
 				}
 			}
 		);
 		rides.toStream().peek( 
 			(key, value) -> log.info("Cheapest Ride => Spaces:" + key.key() + " Company: " + value.getCompany() + " Cost: " + String.format("$ %3d", value.getCost()) + " USD")
 		);
-
 
         return builder.build();
     }
