be4e26b7f973c3b1edd0f1faa2df4209cee78cdf
Hugo Guerrero <1001939+hguerrero@users.noreply.github.com>
2020-04-16 17:34:10 -0400

    update to take env variables

--- amq-html-demo/src/config/mq_config.js
+++ amq-html-demo/src/config/mq_config.js
@@ -1,8 +1,8 @@
 export default (window.config =
     {
-        mq_username: "m_user",
-        mq_password: "m_password",
-        mq_server: "wss://localhost:443",
-        mq_destination: "m_address"
+        mq_username: process.env.MQ_USERNAME || "m_user",
+        mq_password: process.env.MQ_PASSWORD || "m_password",
+        mq_server: process.env.AMQ_URL || "wss://localhost:443",
+        mq_destination: process.env.DESTINATION || "m_address"
     }
 );
